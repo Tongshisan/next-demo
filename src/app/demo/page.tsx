15989da302c42df63c4b5d27422f92df9c7218eb
lizhi <lizhi@aijiaoyi.xyz>
2023-09-20 11:30:55 +0800

    test: test fetch

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -14,18 +14,22 @@ const getData = async () => {
   // console.log('header', headers, headers())
   const token = '-';
   const referer = headers().get('referer')
-  console.log('getData', referer)
+  const res = await fetch('http://jsonplaceholder.typicode.com/posts/2');
+  const data = await res.json();
+  console.log('getData', referer, data)
   await sleep(1);
   return {
     token,
-    referer
+    referer,
+    // data: data.json()
+    data
   }
 }
 
 
 
 const Demo: FunctionComponent<DemoProps> = async () => {
-  const { token, referer } = await getData();
+  const { token, referer, data } = await getData();
 
 
   return (
@@ -33,6 +37,7 @@ const Demo: FunctionComponent<DemoProps> = async () => {
       <h1>{`Demo-${referer}-${token}`}</h1>
       <h2>{token}</h2>
       <h1>{referer}</h1>
+      <div>{data.body}</div>
       <Index referer={referer ?? ''} data={[]} />
     </div>
   );
